cd8494e8a8bd430c354350370d9b7ad6d0340942
Ulises Suarez <ulises203@msn.com>
2018-06-13 17:22:24 +0200

    save carrera

--- src/main/java/org/mvpigs/cotxox/service/CarreraService.java
+++ src/main/java/org/mvpigs/cotxox/service/CarreraService.java
@@ -1,18 +1,27 @@
 package org.mvpigs.cotxox.service;
 
 import org.mvpigs.cotxox.domain.Carrera;
+import org.mvpigs.cotxox.repo.CarreraRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
 public class CarreraService {
-
-	public Long crearCarrera(String string, String string2, String string3, int i, int j) {
-		// TODO Auto-generated method stub
-		return null;
+	@Autowired
+	private CarreraRepo carreraRepo;
+	
+	public Long crearCarrera(String tarjeta, String origen, String destino, int tiempoCarrera, int tiempoEsperado) {
+		Carrera carrera=new Carrera(tarjeta);
+		carrera.setOrigen(origen);
+		carrera.setDestino(destino);
+		carrera.setTiempoCarrera(tiempoCarrera);
+		carrera.setTiempoEsperado(tiempoEsperado);
+		carreraRepo.save(carrera);
+		return carrera.getId();
 	}
 
 	public Carrera recuperaCarrera(Long idCarrera) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return carreraRepo.findOne(idCarrera);
 	}
 
 	public void updateCarrera(Carrera carrera) {
